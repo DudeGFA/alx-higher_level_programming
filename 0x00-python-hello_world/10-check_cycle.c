11a5631e26196e235e41294c410445c31c9bf346
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-06-28 21:54:27 +0100

    Create 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - checks for a cycle in
+ * a singly linked list
+ * @list: singly linked list
+ * Return: 1 or 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *z = list;
+	listint_t *p = list;
+
+	if (!list)
+		return (0):
+	while (p && z)
+	{
+		p = p->next;
+		z = z->next->next;
+		if (p == z)
+			return (1);
+	}
+	return (0);
+}
