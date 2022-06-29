dacd93cb5dd7621b245faad7449a4cadadba7bf8
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-06-30 00:14:34 +0100

    Create 13-insert_number.c

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,18 @@
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *P = *head;
+	listint_t *t;
+	listint_t *newnode;
+
+	newnode = malloc(sizeof(listint_t))
+    if (p == NULL || newnode == NULL)
+        return (NULL);
+    while(p.n < number)
+	{
+		t = p;
+		p = p->next;
+	}
+	t->next = newnode;
+	newnode.next = p;
+	return (newnode);
+)
