e9769f48fa901e36d02d265d28fdff8361415b09
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-06-29 21:52:24 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 a=0
 b=0
 for i in range(1, 80):
-    if sorted(str(i)) == list(str(i)) && ((i // 10) != (i % 10)):
+    if sorted(str(i)) == list(str(i)) and ((i // 10) != (i % 10)):
         print("{:02}".format(i), end=", ")
 print("89")
