18e2d86de2fdc044750ee8e20ea658125f22d6ed
DudeGFA <101369194+DudeGFA@users.noreply.github.com>
2022-06-29 23:18:05 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # function checks if a value is lowercase
 def isupper(str):
-    for i in range(len(str))
+    for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             str[i] = chr(ord(str[i]) - 32)
     print(str)
