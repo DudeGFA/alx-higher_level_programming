72508a59b71f1cab0b287a42e3477c19e48886cb
DudeGFA <favouridowuagida0@gmail.com>
2022-07-06 00:18:13 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,6 +6,8 @@ int is_palindrome(listint_t **head)
     int i, k, j;
     int *arr_of_n;
 
+    if (!head || (*head) == NULL)
+        return (1);
     for(j = 0; child != NULL; j++)
         child = child->next;
     arr_of_n = malloc(j * sizeof(int));
@@ -18,7 +20,9 @@ int is_palindrome(listint_t **head)
         child = child->next;
     }
     for(k = 0, i--; k < (j / 2); k++, i--)
+    {
         if (arr_of_n[k] != arr_of_n[i])
             return (0);
+    }
     return (1);
 }
