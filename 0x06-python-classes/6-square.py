c30a557a4fac4f1d3fd12c1c042c60521a6ba73e
DudeGFA <favouridowuagida0@gmail.com>
2022-07-21 02:49:35 +0100

    debug 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -41,11 +41,12 @@ class Square:
         """prints a square."""
         if self.__size == 0:
             print("")
+            return
         for i in range(self.__position[1]):
             print("")
         for i in range(self.__size):
             for i in range(self.__position[0]):
-                print("_", end="")
+                print(" ", end="")
             for i in range(self.__size):
                 print("#", end="")
             print("")
