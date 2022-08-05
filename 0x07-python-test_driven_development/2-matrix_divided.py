2d1aa6f7b6d7df4e912dd6bdd27490ff9bd89218
DudeGFA <favouridowuagida0@gmail.com>
2022-08-05 05:58:09 +0100

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,16 +15,18 @@ def matrix_divided(matrix, div):
     Returns:
         a new matrix with all elements divided by div.
     """
+    f_err = 'matrix must be a matrix (list of lists) of integers/floats'
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError('div must be a number')
     if div == 0:
         raise ZeroDivisionError('division by zero')
-    if not isinstance(matrix, list) or matrix == []:
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    if not isinstance(matrix, list) or matrix == [] or \
+            (not all(isinstance(row, list) for row in matrix)):
+        raise TypeError(f_err)
     for row in matrix:
-        if len(row) != len(matrix[0]):
-            raise TypeError('Each row of the matrix must have the same size')
         for no in row:
             if not isinstance(no, int) and not isinstance(no, float):
-                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+                raise TypeError(f_err)
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
