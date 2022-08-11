ff65c8a594a2bfdc9918be4bcfbe0d26c4cda106
DudeGFA <favouridowuagida0@gmail.com>
2022-08-11 04:46:06 +0100

    Update base.py
    even more debugging

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -88,7 +88,7 @@ class Base:
             return []
 
     @classmethod
-    def save_to_file(cls, list_objs):
+    def save_to_file_csv(cls, list_objs):
         """Write the CSV serialization
             of a list of objects to a file.
             Args:
