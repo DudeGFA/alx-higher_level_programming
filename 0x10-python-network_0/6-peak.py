e23f5d506a6a2dff059502c78d09fb2a335a1594
DudeGFA <favouridowuagida0@gmail.com>
2022-10-26 22:55:39 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,18 +2,19 @@
 """ Contains function findPeak"""
 
 
-def findPeak(arr) :
-""" finds a peak element in a list"""
+def findPeak(arr):
+    """ finds a peak element in a list"""
+
     n = len(arr)
-    if (n == 1) :
-      return 0
-    if (arr[0] >= arr[1]) :
+    if (n == 1):
         return 0
-    if (arr[n - 1] >= arr[n - 2]) :
+    if (arr[0] >= arr[1]):
+        return 0
+    if (arr[n - 1] >= arr[n - 2]):
         return n - 1
 
     i = n - 1
-    while i > 0 :
+    while i > 0:
         if (arr[i] >= arr[i - 1] and arr[i] >= arr[i + 1]):
             return i
         elif (arr[i] > arr[i - 1]):
