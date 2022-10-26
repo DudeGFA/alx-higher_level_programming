1aeb4046ed7f8e08d4da636700ad8a0c25b051d2
God'sfavour Idowu-Agida <favouridowuagida0@gmail.com>
2022-10-26 10:32:18 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#sends a GET request with a header variable
-#+ and displays the body of the response
+#sends a GET request with a header variable and displays the body of the response
 curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" 
