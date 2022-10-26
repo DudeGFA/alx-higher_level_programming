429a568cb84d2a77efddf7f6461a9abe420476aa
God'sfavour Idowu-Agida <favouridowuagida0@gmail.com>
2022-10-26 10:32:51 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#sends a GET request with a header variable
-#+ and displays the body of the response
+#sends a GET request with a header variable and displays the body of the response
 curl -sH "X-School-User-Id: 98" "${1}"
