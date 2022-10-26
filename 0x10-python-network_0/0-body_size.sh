20265358983ab3121f39522976bd2ff38ca5b7ae
God'sfavour Idowu-Agida <favouridowuagida0@gmail.com>
2022-10-26 22:26:31 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a request and displays the size of the body of the response
-#+ in bytes
+# Sends a request and displays the size of the body of the response in bytes
 curl -s "$1" | wc -c
